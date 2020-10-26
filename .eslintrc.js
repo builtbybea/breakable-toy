653b3c3f5e9bf7bdc6f474e90387a7433437528c
harriet-mcmahon <harrietsotheremail@gmail.com>
2020-10-26 16:37:13 +0000

    fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,24 @@
 module.exports = {
-  env: {
-    'node': true,
-  },
-  extends: [
-    '@fueled/frontend-web/lib/vue',
-  ],
-  'settings': {},
-  'rules': {
-    'no-console': 'off',
-  },
-};
+    env: {
+      'node': true,
+    },
+    extends: [
+      '@fueled/frontend-web/lib/vue',
+    ],
+    settings: {
+      'import/ignore': [
+        'node_modules/',
+      ],
+      'import/resolver': {
+        webpack: {
+          config: require.resolve('@vue/cli-service/webpack.config.js')
+        },
+      },
+    },
+    'rules': {
+      "no-process-env": 0,
+      "vue/no-multiple-template-root": 0,
+      "vue/component-definition-name-casing": 0
+       // "no-console": "off",
+    },
+  };
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Vue.js App"/>
   </div>
 </template>
 
@@ -10,9 +10,9 @@
 import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
+  name: 'home-page',
   components: {
-    HelloWorld
+    HelloWorld,
   }
 }
 </script>
