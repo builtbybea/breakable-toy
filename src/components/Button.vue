6b2a515d7ebbafbfb5aee8366021c7e7650dd071
Beatrice Opara <38406219+builtbybea@users.noreply.github.com>
2020-10-27 12:48:05 +0000

    Update Button.vue
    added template for reusable button

---
+++ src/components/Button.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="button">
+    <button>{{ text }}</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'button',
+  props: {
+    text: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+
+<style scoped>
+
+</style>
