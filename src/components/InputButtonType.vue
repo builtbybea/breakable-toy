7a6a13733ed4ca145780bc9cf1916c11612863dc
Beatrice Opara <38406219+builtbybea@users.noreply.github.com>
2020-11-04 19:07:23 +0000

    created InputButtonType
    created reusable InputButton component

---
+++ src/components/InputButtonType.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <input v-if="type" id="answer" :type="type">
+    <label for="answer">{{ text }}</label>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'input-button-type',
+  props: {
+    text: String,
+    type: String,
+  },
+};
+</script>
+
--- src/views/Quiz.vue
+++ src/views/Quiz.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="quiz">
     <Title msg="What Are Your Favourite Genres To Watch?" />
+    <InputButton text="Action" type="radio" />
   </div>
 </template>
 
 <script>
 import Title from '@/components/Title';
+import InputButton from '@/components/InputButtonType';
 export default {
   name: 'quiz-page',
   components: {
     Title,
+    InputButton,
   },
 };
 </script>
