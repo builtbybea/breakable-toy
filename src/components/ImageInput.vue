46fc50f925eb6a195b71064274d95fed0f975276
Beatrice Opara <38406219+builtbybea@users.noreply.github.com>
2020-11-17 16:06:23 +0000

    wip: Add CustomInput to ImageInput

---
+++ src/components/ImageInput.vue
@@ -0,0 +1,14 @@
+<template>
+  <CustomInput />
+</template>
+
+<script>
+import CustomInput from '@/components/CustomInput';
+
+export default {
+  name: 'image-input',
+  components: {
+    CustomInput,
+  },
+};
+</script>
