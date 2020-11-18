386edc98fc6a8160c850d3685386e561cd23ba5d
Beatrice Opara <38406219+builtbybea@users.noreply.github.com>
2020-11-18 13:22:27 +0000

    wip: Add choices in mapInputs

--- src/components/ImageInput.vue
+++ src/components/ImageInput.vue
@@ -1,5 +1,18 @@
 <template>
-  <CustomInput />
+  <div class="image-input">
+    <img :src="image" :alt="alt"> 
+    <input
+      :id="text" 
+      :value="text" 
+      class="image-input__button" 
+      :type="type" 
+      :name="name"
+    >
+    <label v-if="type === image-selection" :for="text">
+      {{ text }}
+    </label>
+    <CustomInput v-if="type === image-radio" />
+  </div>
 </template>
 
 <script>
@@ -10,5 +23,14 @@ export default {
   components: {
     CustomInput,
   },
+  props: {
+    text: String,
+    type: String,
+    name: String,
+    image: String,
+    alt: String,
+  },
 };
+//v-if type === image-selection, show image, paragraph 
+//v-else type === image-radio, show image and radio choice 
 </script>
--- src/mapInputs.js
+++ src/mapInputs.js
@@ -1,7 +1,8 @@
 export const inputType = {
   'multi-answer-multi-choice': 'checkbox',
-  'image-choice': 'radio',
+  'image-selection': 'radio',
   'single-answer-multi-choice': 'radio',
+  'image-radio': 'radio',
   'slider-scale': 'range',
   'star-rating': 'radio',
 };
