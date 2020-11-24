0ce50905202bdbbf8d029d4264926e58addbc5a7
Beatrice Opara <38406219+builtbybea@users.noreply.github.com>
2020-11-24 14:00:56 +0000

    refactor: Wrap input, img and h3 in label tag
    Fix functionality to use image as radio button

--- src/components/ImageInput.vue
+++ src/components/ImageInput.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="image-input">
-    <input 
+    <!-- <input 
       :id="text" 
       :value="text" 
       class="image-input__button" 
@@ -9,7 +9,23 @@
     >
     <img class="image-input__image" :src="image" :alt="text"> 
     <label :for="text" :class="['image-input__label', {'image-input__label--radio' :type==='radio'}]">
-      <h3>{{ text }}</h3>
+      <h3 class="image-input--text">{{ text }}</h3>
+    </label> -->
+
+
+
+   
+    
+    <label :for="text" :class="['image-input__label', {'image-input__label--radio' :type==='radio'}]">
+      <input 
+        :id="text" 
+        :value="text" 
+        class="image-input__button" 
+        :type="type" 
+        :name="name"
+      >
+      <img class="image-input__image" :src="image" :alt="text"> 
+      <h3 class="image-input--text">{{ text }}</h3>
     </label>
   </div>
 </template>
@@ -25,7 +41,5 @@ export default {
     image: String,
     alt: String,
   },
-};
-//v-if type === image-selection, show image, paragraph 
-//v-else type === image-radio, show image and radio choice 
+}; 
 </script>
