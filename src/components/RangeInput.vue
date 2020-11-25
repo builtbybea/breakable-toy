75a9bfeedd1d9f7241d2eca65dc68cfeff5df26a
Beatrice Opara <38406219+builtbybea@users.noreply.github.com>
2020-11-25 11:19:07 +0000

    wip: Create RangeInput component

---
+++ src/components/RangeInput.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="range-input"> 
+    <input
+      :id="text"
+      :value="text"
+      class="range-input__button"
+      :type="type"
+      :name="name"
+      min="0"
+      max="100"
+    >
+    <label :for="text" class="" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'range-input',
+  props: {
+    text: String,
+    type: String,
+    name: String,
+  },
+};
+</script>
+
