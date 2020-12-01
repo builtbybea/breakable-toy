2897507ef5f7b02f38641a7dccfd470678b8d36e
Beatrice Opara <38406219+builtbybea@users.noreply.github.com>
2020-12-01 12:32:22 +0000

    wip: Add background color to slider background

--- src/components/RangeInput.vue
+++ src/components/RangeInput.vue
@@ -1,21 +1,23 @@
 <template>
   <div class="range-input"> 
     <div class="range-input__wrapper">
-      <input
-        :id="text"
-        v-model="value"
-        class="range-input__button"
-        :type="type"
-        :name="name"
-        min="0"
-        max="100"
-        @change="selectedRange"
-      >
-      <label :for="text" class="range-input__label" />
-      <div class="range-input__slider">
-        <span class="range-input__emoji-thumb"> {{ getRating }} </span>
-        <span> {{ value }} </span>
-      </div>
+      <label :for="text" class="range-input__label" :style="getBackground">
+        <input
+          :id="text"
+          v-model="value"
+          class="range-input__button"
+          :type="type"
+          :name="name"
+          min="0"
+          max="100"
+          @change="selectedRange"
+        >
+        <!-- <label :for="text" class="range-input__label" :style="getSliderBackground" /> -->
+        <div class="range-input__slider" :style="getBackgroundSlider">
+          <span class="range-input__emoji-thumb"> {{ getRating }} </span>
+          <span> {{ value }} </span>
+        </div>
+      </label>
     </div>
   </div>
 </template>
@@ -31,11 +33,19 @@ export default {
   data(){
     return {
       value: 0,
-      // testerStyle: {
-      //   background: 'red',
-      //   height: '6px',
-      //   borderRadius: '10px',
-      // },
+      testerStyle: {
+        position: 'relative',
+        display: 'flex',
+        width: '100%',
+        background: '#c4c4c4',
+        height: '6px',
+        borderRadius: '10px',
+        zIndex: '2',
+      },
+      testerBackground: {
+        background: '#ef597a',
+        zIndex: '1',
+      },
     };
   },
   computed: {
@@ -47,10 +57,17 @@ export default {
       const ratings = ['💩','🙁','😐','🙂','😊','😍'];
       return ratings[(Math.floor(this.value/20))];
     },
+    getBackground(){
+      const color = this.testerStyle;
+      return color;
+    },
   },
   methods: {
     selectedRange(value) {
       this.$emit('get-range', value);
+    },
+    getBackgroundSlider() {
+      return this.testerBackground;
     },
   },
 };
