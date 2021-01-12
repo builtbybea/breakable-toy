54caf04b170193c5e0c772f6b44ab3a8e423f768
Beatrice Opara <38406219+builtbybea@users.noreply.github.com>
2021-01-12 14:11:02 +0000

    wip: created feature to get character with highest score

--- src/views/Results.vue
+++ src/views/Results.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="results">
     <Title msg="You Are..." />
+    <h3>{{ characterName }}</h3>
   </div>
 </template>
 
@@ -10,6 +11,47 @@ export default {
   name:'results-page',
   components: {
     Title,
+  },
+  data() {
+    return {
+      totalScore: {
+        'Sailor Moon': 5,
+        'Spike Spiegel': 0,
+        'Goku': 10,
+        'Michiko': 5,
+        'Monkey D. Luffy': 20,
+      },
+    };
+  },
+  computed: {
+    // reference for .reduce() answer https://stackoverflow.com/questions/27376295/getting-key-with-the-highest-value-from-object
+    // characterNameLoop() {
+    //   const testArray = ['Helen', 'Florence', 'Perpetua'];
+    //   for(let index = 0; testArray.length > index; index++){
+    //     console.log(index, testArray[index]);
+    //   }
+    //   testArray.forEach((value, index) => {
+    //     console.log(value, index);
+    //   });
+    //   return 'test';
+    // },
+    characterName(){
+      //created a variable that stores an array of keys which are the character names
+      const characterNames = Object.keys(this.totalScore);
+      //created a variable that stores the first value from the array characterNames
+      let highestScoreName = characterNames[0];
+      //loop through characterName array, which runs a function that passes a parameter called key(key is a string)
+      characterNames.forEach((key) => {
+        //TODO: Add randomizer when tie-break
+        //checks if the score of the key(or character) is higher than the current high score
+        if(this.totalScore[key] > this.totalScore[highestScoreName]) {
+          //set the highestScoreName to be the new key (or string)
+          highestScoreName = key;
+        }
+      });
+      //returns the name of the character with the highest score
+      return highestScoreName;
+    },
   },
 };
 </script>
