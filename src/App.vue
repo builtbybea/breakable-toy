7219427179c13b19520824d58148e84e2a89e929
Beatrice Opara <38406219+builtbybea@users.noreply.github.com>
2021-03-02 10:32:05 +0000

    debug purposes

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
-  <!-- <pre class="_debug">
+  <pre class="_debug">
     {{ $store.state.quiz.userAnswers }}
-    </pre> -->
+    </pre>
   <div class="main">
     <Nav />
     <router-view />
@@ -29,15 +29,15 @@ export default {
   text-align: center;
   color: #2c3e50;
 }
-// ._debug {
-//   position: fixed;
-//   top: 0;
-//   right: 0;
-//   padding: 5px;
-//   background: rgba(black, 0.8);
-//   z-index: 9999;
-//   margin: 0;
-//   color: white;
-// }
+._debug {
+  position: fixed;
+  top: 0;
+  right: 0;
+  padding: 5px;
+  background: rgba(black, 0.8);
+  z-index: 9999;
+  margin: 0;
+  color: white;
+}
 
 </style>
